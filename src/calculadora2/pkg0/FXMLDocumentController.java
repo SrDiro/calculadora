8800b6c80ae0f53b7c032f3e6f18b8a47ce92559
Rasul <Rasul@192.168.1.244>
2017-03-01 23:05:24 +0100

    calca1.0

---
+++ src/calculadora2/pkg0/FXMLDocumentController.java
@@ -0,0 +1,56 @@
+package calculadora2.pkg0;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class FXMLDocumentController implements Initializable {
+    @FXML
+    private TextField pantalla;
+    @FXML
+    private Button siete;
+    @FXML
+    private Button ocho;
+    @FXML
+    private Button tres;
+    @FXML
+    private Button dos;
+    @FXML
+    private Button cinco;
+    @FXML
+    private Button nueve;
+    @FXML
+    private Button cuactro;
+    @FXML
+    private Button uno;
+    @FXML
+    private Button seis;
+    @FXML
+    private Button mas;
+    @FXML
+    private Button menos;
+    @FXML
+    private Button dividir;
+    @FXML
+    private Button multiplicar;
+    @FXML    
+    private Button igual;
+    
+        
+    @FXML
+    private void calcular(ActionEvent event) {
+    }
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    
+}
